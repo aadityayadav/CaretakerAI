b0121e7ea626495833ea87b2ca46640f545a0413
Xavier D'Mello <xaviermdmello@gmail.com>
2024-11-24 02:43:07 -0500

    update ui looks better

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,7 +7,7 @@ import "@fontsource/montserrat"; // Import Montserrat font
 import { extendTheme } from "@chakra-ui/react";
 import { Box } from "@chakra-ui/react";
 import Header from "./components/header";
-
+import { Image } from "@chakra-ui/react";
 const theme = extendTheme({
   fonts: {
     heading: "Montserrat, sans-serif",
@@ -25,21 +25,46 @@ export default function RootLayout({
       <body>
         <ChakraProvider theme={theme}>
           <Providers>
-            <Box
-              background="radial-gradient(circle, rgba(220,220,235,1) 0%, rgba(230,230,255,1) 100%)"
-              minHeight="100vh"
-              height="100%"
-              padding={4}
-            >
+            <Box position="relative" minHeight="100vh">
+              <Image
+                src="/hero-top.jpg"
+                alt="hero"
+                position="absolute"
+                top="0"
+                left="0"
+                width="100%"
+                height="100%"
+                objectFit="cover"
+                zIndex={0}
+              />
               <Box
-                maxWidth="1200px"
-                margin="0 auto"
-                display="flex"
-                flexDirection="column"
-                gap={4}
+                position="relative"
+                background="transparent"
+                minHeight="100vh"
+                height="100%"
+                padding={4}
               >
-                <Header />
-                {children}
+                <Box
+                  maxWidth="1200px"
+                  margin="0 auto"
+                  display="flex"
+                  flexDirection="column"
+                  gap={4}
+                >
+                  <Header />
+                  <Box
+                    borderRadius="10px"
+                    backdropFilter="blur(16px)"
+                    backgroundColor="rgba(255, 255, 255, 0.35)"
+                    border="1px solid rgba(255, 255, 255, 0.2)"
+                    padding={4}
+                    minHeight="100vh"
+                    height="100%"
+                    boxShadow="0 4px 6px rgba(0, 0, 0, 0.1)"
+                  >
+                    {children}
+                  </Box>
+                </Box>
               </Box>
             </Box>
           </Providers>
